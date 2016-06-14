fd73abe3e40f60302fc10ff939258afa9217d650
dengchao <dengchao@zenmen.com>
2016-06-14 10:43:35 +0800

    add CompatModeChinaDemo project with android-studio.

---
+++ sample/android-studio/CompatModeChinaDemo/app/src/main/java/com/igeak/android/test/FunctionTestService.java
@@ -0,0 +1,62 @@
+package com.igeak.android.test;
+
+import com.igeak.android.common.api.GeakApiClient;
+import com.igeak.android.common.api.ResultCallback;
+import com.igeak.android.wearable.DataApi;
+import com.igeak.android.wearable.DataApi.GetFdForAssetResult;
+import com.igeak.android.wearable.DataEvent;
+import com.igeak.android.wearable.DataEventBuffer;
+import com.igeak.android.wearable.DataItem;
+import com.igeak.android.wearable.DataItemAsset;
+import com.igeak.android.wearable.MessageEvent;
+import com.igeak.android.wearable.Wearable;
+import com.igeak.android.wearable.WearableListenerService;
+
+import java.io.InputStream;
+
+public class FunctionTestService extends WearableListenerService {
+    
+    private GeakApiClient client;
+    
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        client = new GeakApiClient.Builder(this).addApi(Wearable.API).build();
+        client.connect();
+    }
+    
+    @Override
+    public void onDataChanged(DataEventBuffer buffer) {
+        if (buffer != null && buffer.getCount() > 0) {
+            DataEvent e = buffer.get(0);
+            if (e.getDataItem() != null) {
+                DataItem item = e.getDataItem();
+                if (item.getData() != null && item.getData().length > 0) {
+                    Utils.setText(this, "receive",
+                            "" + Utils.getHashCode(item.getData()));
+                } else if (item.getAssets().containsKey("key")) {
+                    DataItemAsset a = item.getAssets().get("key");
+                    Wearable.DataApi.getFdForAsset(client, a).setResultCallback(
+                            new ResultCallback<DataApi.GetFdForAssetResult>() {
+                        @Override
+                        public void onResult(GetFdForAssetResult result) {
+                            if (result.getStatus().isSuccess()) {
+                                InputStream in = result.getInputStream();
+                                Utils.setText(FunctionTestService.this, "receive",
+                                        "" + Utils.readAll(in));
+                            }
+                        }
+                    });
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onMessageReceived(MessageEvent messageevent) {
+        if (messageevent != null && messageevent.getData() != null) {
+            Utils.setText(this, "receive", "" + Utils.getHashCode(messageevent.getData()));
+        }
+    }
+    
+}
---
+++ sample/android-studio/CompatModeChinaDemo/app/src/main/java/com/igeak/android/test/Utils.java
@@ -0,0 +1,57 @@
+package com.igeak.android.test;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Utils {
+    
+    public static final String INTENT_TAG = "FunctionTestActivity";
+    
+    public static void setText(Context context, String key, String value) {
+        Intent intent = new Intent(INTENT_TAG);
+        Bundle bundle = new Bundle();
+        bundle.putString(key, value);
+        intent.putExtras(bundle);
+        context.sendBroadcast(intent);
+    }
+    
+    
+    public static int readAll(InputStream in) {
+        int code = 0;
+        byte[] buffer = new byte[1024];
+        while (true) {
+            try {
+                int len = in.read(buffer);
+                for (int i = 0; i < len; i++) {
+                    code += buffer[i];
+                }
+                if (len < 0) break;
+            } catch (IOException e) {
+                Log.e(FunctionTestActivity.TAG, e.getMessage(), e);
+            }
+        }
+        return code;
+    }
+    
+    public static byte[] getData(int len) {
+        byte[] b = new byte[len];
+        for (int i = 0; i < len; i++) {
+            b[i] = (byte)(Math.random() * 256);
+        }
+        return b;
+    }
+    
+    public static int getHashCode(byte[] data) {
+        int code = 0;
+        for (byte b : data) {
+            code += b;
+        }
+        return code;
+    }
+
+}
