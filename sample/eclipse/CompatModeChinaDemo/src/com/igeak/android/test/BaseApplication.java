361a01721479b1c4d9037d1791d696d6fe0d07aa
dengchao <dengchao@zenmen.com>
2016-06-14 10:07:53 +0800

    add CompatModeChinaDemo in Eclipse.

---
+++ sample/eclipse/CompatModeChinaDemo/src/com/igeak/android/test/BaseApplication.java
@@ -0,0 +1,29 @@
+package com.igeak.android.test;
+
+import android.app.Application;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.igeak.android.common.GeakApiManager;
+import com.igeak.android.common.NoAvailableServiceException;
+
+public class BaseApplication extends Application {
+
+    public static final String TAG = "FunctionTest";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (!GeakApiManager.getInstance().isInitialized()) {
+            try {
+                GeakApiManager.getInstance().adaptService(this);
+            } catch (NoAvailableServiceException e) {
+                Log.e(TAG, "no avaliable service.", e);
+                Toast.makeText(this, "neither mms nor gms is avaliable.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+        Log.i(TAG, "Application created");
+    }
+
+}
